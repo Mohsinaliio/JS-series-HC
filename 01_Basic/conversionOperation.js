5d9ac7dde18b6576b72c5422b6fa8b7704b3afd6
Mohsin Ali <124897971+Mohsinaliio@users.noreply.github.com>
2024-06-03 13:00:33 +0000

    datatype conversio

---
+++ 01_Basic/conversionOperation.js
@@ -0,0 +1,14 @@
+let score = "mohsin";
+
+// console.log(typeof score);
+// console.log(typeof(score));
+
+let valueInNumber = Number(score);
+// console.log(typeof valueInNumber);
+// console.log(valueInNumber);
+
+
+let isLoggedIn = "false"
+
+let booleanIsLoggedIn = Boolean(isLoggedIn);
+console.log(booleanIsLoggedIn)
